fc6c88c6923be22a7d7a65cd222d9b2dad458071
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2023-04-11 13:25:34 +0700

    middleware admin, dashboard user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,18 +10,19 @@ class HomeController extends Controller
     //
     public function dashboard()
     {
-        // switch (Auth::user()->is_admin) {
-        //     case true:
-        //         return redirect(route('admin.dashboard'));
-        //         break;
+        switch (Auth::user()->is_admin) {
+            case true:
+                return redirect(route('admin.dashboard'));
+                break;
             
-        //     default:
-        //         return redirect(route('user.dashboard'));
-        //         break;
-        // }
-        $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
-        return view('user.dashboard',[
-            'checkouts' => $checkouts
-        ]);
+            default:
+                echo "masuk kesini";
+                return redirect(route('user.dashboard'));
+                break;
+        }
+        // $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
+        // return view('user.dashboard',[
+        //     'checkouts' => $checkouts
+        // ]);
     }
 }
