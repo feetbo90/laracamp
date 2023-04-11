ab70e83e825e241344fc0a6e91e57fa46be7cce0
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2023-04-11 13:42:15 +0700

    paid checkout

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -15,7 +15,7 @@ class CheckoutController extends Controller
         $checkout->save();
         
         // send email to user
-        // Mail::to($checkout->User->email)->send(new Paid($checkout));
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
 
         $request->session()->flash('success', "Checkout with ID {$checkout->id} has been updated!");
         return redirect(route('admin.dashboard'));
